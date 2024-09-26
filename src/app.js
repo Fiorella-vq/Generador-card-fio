b64d3d2c256dc0813f0c38d59a40f22867047375
Fiorella-vq <fio.89@hotmail.com>
2024-09-25 21:32:57 -0300

    card aleatoria

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let cardValues= [2,3,4,5,6,7,8,9,10,"A","J","Q","K"] ;
+let cardPintas=  ["♦","♥","♠","♣"] ;
+
+function generadorCard(arr){
+Math.floor(Math.random()*cardValues.length);
+return arr [Math.floor(Math.random()*arr.length)] ;
+}
+const cardaleatorio = generadorCard (cardValues);
+
+document.querySelector(".value").innerHTML = cardaleatorio;
+
+const  pintaleatorio = generadorCard (pintaleatorio);
+document.querySelector
+
 };
